61a13a591fccbb8573148d35d6e7d9acfc78f6b6
Tomasz-Meres <tomaszmeres@interia.pl>
2025-11-13 15:36:19 +0100

    Stwory

---
+++ Simulator/Creature.cs
@@ -0,0 +1,26 @@
+﻿namespace Simulator;
+
+public class Creature
+{
+    public string Name { get; set; }
+    public int Level { get; set; }
+
+    public Creature()
+    {
+    }
+
+    public Creature(string name, int level = 1)
+    {
+        Name = name;
+        Level = level;
+    }
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+    }
+
+    public string Info
+    {
+        get { return $"{Name} [{Level}]"; }
+    }
+}
